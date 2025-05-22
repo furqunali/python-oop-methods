d672374b5a158af38eac3789d94a9c1c7fd8a69a
furqunali <furqanali2628@gmail.com>
2025-05-22 07:16:45 +0500

    Add files via upload

---
+++ Oops3.py
@@ -0,0 +1,20 @@
+#Methods are funtions that belong to objects.
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+            print("hi", self.name, "your avg score is", sum/3)
+
+s1 = Student("Zeeshan Khan", [99, 90, 92])
+s1.get_avg()    
+
+# Static Method
+
+#S@staticmethod
